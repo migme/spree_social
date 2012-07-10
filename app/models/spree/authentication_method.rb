fa416b3ad7ac70f67e917dd662a66c0efaf09068
Quy Doan <quydoantran@gmail.com>
2012-07-10 15:17:03 +0800

    update authentication_method

--- app/models/spree/authentication_method.rb
+++ app/models/spree/authentication_method.rb
@@ -1,3 +1,13 @@
 class Spree::AuthenticationMethod < ActiveRecord::Base
   attr_accessible :provider, :api_key, :api_secret, :environment, :active
+
+  scope :current_env, where(:environment => ::Rails.env)
+
+  class << self
+    def get_auth(provider)
+      current_env.where(:provider => provider).first
+    end
+  end
+
+
 end
