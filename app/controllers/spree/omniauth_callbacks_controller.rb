56aea7ab84c2cdeb8212bdf2cb67ff18b225a09f
Quy Doan <quydoantran@gmail.com>
2012-09-05 15:04:37 +0800

    add sign up token

--- app/controllers/spree/omniauth_callbacks_controller.rb
+++ app/controllers/spree/omniauth_callbacks_controller.rb
@@ -1,5 +1,6 @@
 class Spree::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   #include Spree::Core::ControllerHelpers
+  helper 'spree/users', 'spree/base'
 
   def facebook
     if request.env["omniauth.error"].present?
@@ -14,27 +15,30 @@ class Spree::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       @user = authentication.user
       if @user && !@user.confirmed?
         @user.completed_sign_up!
+        fire_event('spree.user.signup', :user => @user, :order => current_order(true))
         session[:just_activated] = true
         session[:registering_user_id] = nil
       end
       sign_in(:user, @user)
     else
       if current_user
-        current_user.completed_sign_up! if Spree::User::INCOMPLETE_STATES.include?(current_user.state_name)
         current_user.user_authentications.create!(:provider => auth_hash['provider'], :uid => auth_hash['uid'])
         flash[:success] = "Authentication successful."
         @user = current_user
       else
         # Register new account
         @user = Spree::User.apply_omniauth(auth_hash, session[:affiliate_code])
-        sign_in(:user, @user)
+        if @user.persisted?
+          sign_in(:user, @user)
+          fire_event('spree.user.signup', :user => @user, :order => current_order(true))
+        end
       end
       session[:registering_user_id] = nil
       if !@user.has_role?('auction_user')
         session[:just_activated] = true
       end
     end
-    redirect_to(root_url)
+    redirect_to root_url(:subdomain => nil)
   end
 
   def failure
